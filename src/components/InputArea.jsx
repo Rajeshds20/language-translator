4cc71550d435c4a459f38e852a9f55d6c4a52cb7
Devangam Sajja Rajesh <104376917+Rajeshds20@users.noreply.github.com>
2024-01-18 11:16:35 +0530

    Restrict Resize textarea

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -7,7 +7,7 @@ function InputArea(props) {
     }
     return (
         <div className='input-text'>
-            <textarea onKeyDown={handleKeyDown} type='text' title={props.readOnly ? 'Translated Text' : 'Original Text'} style={{ width: '300px', height: '300px', fontSize: '20px', fontFamily: 'sans-serif', padding: '10px' }} readOnly={props.readOnly} className='input-area' placeholder='Type your text here' onChange={props.onChange} value={props.text} />
+            <textarea onKeyDown={handleKeyDown} type='text' title={props.readOnly ? 'Translated Text' : 'Original Text'} style={{ width: '300px', height: '300px', fontSize: '20px', fontFamily: 'sans-serif', padding: '10px', resize: 'none' }} readOnly={props.readOnly} className='input-area' placeholder='Type your text here' onChange={props.onChange} value={props.text} />
             {
                 !props.readOnly && <div className='input-'>
                     <span className='paste-icon' title='Paste' onClick={() => {
