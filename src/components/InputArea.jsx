5c8dac9b85b015f418c432771f40cb46bee9e0c4
Devangam Sajja Rajesh <104376917+Rajeshds20@users.noreply.github.com>
2024-01-18 13:00:07 +0530

    Updated Input Area

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -7,7 +7,11 @@ function InputArea(props) {
     }
     return (
         <div className='input-text'>
-            <textarea onKeyDown={handleKeyDown} type='text' title={props.readOnly ? 'Translated Text' : 'Original Text'} style={{ width: '300px', height: '300px', fontSize: '20px', fontFamily: 'sans-serif', padding: '10px', resize: 'none' }} readOnly={props.readOnly} className='input-area' placeholder='Type your text here' onChange={props.onChange} value={props.text} />
+            <textarea onKeyDown={handleKeyDown} type='text'
+                className='input-area'
+                title={props.readOnly ? 'Translated Text' : 'Original Text'}
+                readOnly={props.readOnly} placeholder={props.readOnly ? 'Translated text here' : 'Enter your text here'}
+                onChange={props.onChange} value={props.text} />
             {
                 !props.readOnly && <div className='input-'>
                     <span className='paste-icon' title='Paste' onClick={() => {
@@ -15,7 +19,6 @@ function InputArea(props) {
                             props.onChange({ target: { value: text } });
                         });
                     }}>📋</span>
-
                 </div>
             }
         </div>
